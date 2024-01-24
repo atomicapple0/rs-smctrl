30b1ec1ed006ab0067b99f68d87d45550708a358
atomicapple0 <brianzhangaa@gmail.com>
2024-01-24 12:56:57 -0500

    use hacky build script

--- build.rs
+++ build.rs
@@ -1,13 +1,28 @@
-fn main() {
-    println!("cargo:rustc-link-lib=dylib=cuda");
+use std::{env, path::Path, process::Command};
 
-    let src = ["src/libsmctrl.c"];
-    let mut builder = cc::Build::new();
-    let build = builder
-        .files(src.iter())
-        .include("/usr/local/cuda/include")
-        .flag("-Wno-unused-parameter")
-        .flag("-Wno-parentheses")
-        .flag("-Wno-sign-compare");
-    build.compile("libsmctrl");
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    // Note that there are a number of downsides to this approach
+    // See: https://doc.rust-lang.org/cargo/reference/build-script-examples.html
+    Command::new("gcc")
+        .args(&[
+            "src/libsmctrl.c",
+            "-c",
+            "-fPIC",
+            "-lcuda",
+            "-I/usr/local/cuda/include",
+            "-o",
+        ])
+        .arg(&format!("{}/libsmctrl.o", out_dir))
+        .status()
+        .unwrap();
+    Command::new("ar")
+        .args(&["rcs", "libsmctrl.a", "libsmctrl.o"])
+        .current_dir(&Path::new(&out_dir))
+        .status()
+        .unwrap();
+    println!("{}", out_dir);
+
+    println!("cargo:rustc-link-search={}", out_dir);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,14 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(unused)]
+extern crate cudarc;
 include!("./bindings.rs");
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_libsmctrl_set_global_mask() {
+        unsafe { libsmctrl_set_global_mask(0) };
+    }
+}
