f0e7d01dd84cbfd8927cc00c1d9f51f510bc7d32
atomicapple0 <brianzhangaa@gmail.com>
2024-01-23 22:21:10 -0500

    update build.rs

--- build.rs
+++ build.rs
@@ -1,12 +1,13 @@
 fn main() {
+    println!("cargo:rustc-link-lib=dylib=cuda");
+
     let src = ["src/libsmctrl.c"];
     let mut builder = cc::Build::new();
     let build = builder
         .files(src.iter())
-        .include("include")
+        .include("/usr/local/cuda/include")
         .flag("-Wno-unused-parameter")
         .flag("-Wno-parentheses")
-        .flag("-Wno-sign-compare")
-        .define("USE_ZLIB", None);
+        .flag("-Wno-sign-compare");
     build.compile("libsmctrl");
 }
